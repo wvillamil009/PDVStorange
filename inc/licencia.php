8bf2d00b205cac5433587c0d2d8b06e0403b628c
wvillamil009 <wvillamil009@gmail.com>
2023-08-24 17:20:45 -0500

    Ok proyecto

---
+++ inc/licencia.php
@@ -0,0 +1,22 @@
+<br><br>
+<div class="text-center">
+                  <h4 class="mt-1 mb-5 pb-1">Adquirir ahora <b>Licencia PRO</b></h4>
+                  <br><br>
+                  <h3 class="mt-1 mb-5 pb-1">Whatsapp <b>+57 3118869000</b> </h3>
+                  <a class="text-center" href="https://api.whatsapp.com/send?phone=573118869000&text=Me%20interesa%20licencia%20Pro%20Software%20PDV%20Storange" target="_blank" class="link-success">!Chatear ahora!</a>
+                </div>
+
+
+                <!--Start of Tawk.to Script-->
+                <script type="text/javascript">
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/609408cbb1d5182476b655a3/1f5144n4n';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+                })();
+                </script>
+                <!--End of Tawk.to Script-->
